ff9b52eb4f6c63cfae2b34d7b6749b1bfa8cad83
sandipkurmi <sandipkurmi0@gmail.com>
2026-03-21 10:39:11 +0530

    Add calculatePercentage function and corresponding test case

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,8 @@
 export const calculateDiscount = (price: number, discount: number) => {
     return ((price - discount) / price) * 100;
 };
+
+// Calculate percentage
+export const calculatePercentage = (price: number, discount: number) => {
+    return ((price - discount) / price) * 100;
+};
